b4d880d941c3c7f12c9537f1716471b05b81317c
Noah Sheldon <36855642+noah-sheldon@users.noreply.github.com>
2023-11-01 13:31:30 +0000

    Update inverted_symetrical.py

--- patterns/inverted_symetrical.py
+++ patterns/inverted_symetrical.py
@@ -2,7 +2,7 @@ n=5
 for i in range(n):
     for j in range(i):
         print(" ",end="")
-    for j in range(n-i):
+    for j in range(2*(n-i)-1):
         print("* ", end="")
     for j in range(i):
         print(" ",end="")
