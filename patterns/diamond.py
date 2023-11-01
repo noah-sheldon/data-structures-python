64cc4dc7241e3e65948656d3bc5b024545214660
Noah Sheldon <36855642+noah-sheldon@users.noreply.github.com>
2023-11-01 13:30:41 +0000

    Update patterns

---
+++ patterns/diamond.py
@@ -0,0 +1,21 @@
+n=5
+
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("* ", end="")
+    for j in range(n-1-i):
+        print(" ",end="")
+        
+    print("\n")
+    
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(2*(n-i)-1):
+        print("* ", end="")
+    for j in range(i):
+        print(" ",end="")
+        
+    print("\n")
